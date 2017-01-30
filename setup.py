4541099feddca1fff82ddda66659f40f28fa1a20
gojefferson <jeff@lawprct.com>
2017-01-30 13:07:33 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os.path
+
+import setuptools
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+description = "A Python wrapper for the HubSpot contacts and contact properties API"
+
+with open(os.path.join(root_dir, 'README.rst')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='hubbypy',
+    version='1.1',
+    description=description,
+    long_description=long_description,
+    url='https://github.com/gojefferson/hubbypy',
+    author='Jeff Kerr',
+    author_email='jeff@casefleet.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    py_modules=[
+        'hubbypy',
+    ],
+)
